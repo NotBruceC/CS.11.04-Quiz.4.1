f6fd3889405fd8d1275a0329e858bf87f4e08f9f
NotBruceC <brucec2026@student.cis.edu.hk>
2023-11-06 12:17:25 +0800

    aksbkjgadks

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,17 @@ public class Main {
 
     /** 1. Write a method called numberOfVowels that returns the number of vowels in a string.
      Consider a, e, i, o, u as vowels. **/
-    
+    public static int numberOfVowels(String str){
+        int count = 0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='a'||str.charAt(i)=='e'||str.charAt(i)=='i'||str.charAt(i)=='o'||str.charAt(i)=='u'){
+                count++;
+            }else {
+                //do nothing
+            }
+        }
+        return count;
+    }
 
     /** 2. Write a method called notDivisibleBy235 that finds the number of natural numbers not exceeding N
      and not divisible by any of the numbers [2, 3, 5].
@@ -15,6 +25,17 @@ public class Main {
      5 - divides integer by 5
      Answer: 1 (only one number doesn't divide integer by any of 2, 3, 5) **/
 
+    public static int notDivisibleBy235(int N){
+        int count = 0;
+        for(int i=0;i<=N;i++){
+            if(i%2!=0 && i%3!=0 && i%5!=0){
+               count++;
+            }else{
+                //do nothing
+            }
+        }
+        return count;
+    }
 
     /** 3. Write a method called camelCaseMe so that it converts dash/underscore delimited words into camel casing.
      * The first word within the output should be capitalized only if the original word was capitalized
@@ -24,6 +45,27 @@ public class Main {
      "the-stealth-warrior" gets converted to "theStealthWarrior"
      "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
      "The_Stealth-Warrior" gets converted to "TheStealthWarrior" **/
-
-
+    public static String camelCaseMe(String str) {
+        String result = "";
+        for (int i = 0; i < str.length()-1; i++) {
+            if (i!=str.length()-1 && (str.charAt(i) == '-' || str.charAt(i) == '_')) {
+                char ok = str.charAt(i + 1);
+                char ok2 = Character.toUpperCase(ok);
+                i++;
+                result = result + ok2;
+            }else if (i!=str.length()-1 && (str.charAt(i) != '-' || str.charAt(i) != '_')) {
+                result = result + str.charAt(i);
+            }
+        }
+        if(str.charAt(str.length()-1) != '-' && str.charAt(str.length()-1) != '_'){
+            result = result + str.charAt(str.length()-1);
+        }
+        return result;
+    }
+// this took too long
+    // also hi
 }
+
+
+
+
